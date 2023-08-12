155e58568a090a520cba3a5fb46dd0e6b5a34278
Brayan Diaz C <brayandiazc@gmail.com>
2023-08-12 09:29:47 -0500

    Feat:(Contact scaffold) Punto de partida

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :contacts
   get '/home',    to: 'pages#home'
   get '/privacy', to: 'pages#privacy'
   get '/legal',   to: 'pages#terms'
-  root "pages#home"
+  root 'pages#home'
 end
