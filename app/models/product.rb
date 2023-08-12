fd8e411580303132c82dd47ea801d2847a6a7a34
Brayan Diaz C <brayandiazc@gmail.com>
2023-08-12 11:48:10 -0500

    Feat:(Product Validacion) validamos el backend

--- app/models/product.rb
+++ app/models/product.rb
@@ -15,6 +15,19 @@
 #  updated_at      :datetime         not null
 #
 class Product < ApplicationRecord
+  # Relaciones
   belongs_to :user
   belongs_to :category
+
+  # Validaciones
+  validates :name,            presence: true,
+                              uniqueness: true
+  validates :description,     presence: true,
+                              length: { minimum: 10 }
+  validates :price,           presence: true,
+                              numericality: { greater_than: 0 }
+  validates :release_date,    presence: true
+  validates :link_to_website, presence: true,
+                              format: { with: /\A#{URI::DEFAULT_PARSER.make_regexp(%w[http https])}\z/ },
+                              uniqueness: true
 end
